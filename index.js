6cd9389329f93266656bc95e2e1d941570e8f119
Jennifer Rosa <rosajen27@gmail.com>
2020-07-17 12:16:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 inquirer.prompt([/* Pass your questions in here */], function( answers ) {
     // Use user feedback for... whatever!!
