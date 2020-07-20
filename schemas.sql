d400b8c461c6acef044faeb814fd3e1bd60fe46c
Jennifer Rosa <rosajen27@gmail.com>
2020-07-20 19:20:30 -0400

    Create schemas (department, role, employee)

---
+++ schemas.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+	id int NOT NULL AUTO_INCREMENT,
+    name varchar(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+	id int NOT NULL AUTO_INCREMENT,
+    title varchar(30) NOT NULL,
+    salary decimal(10, 3),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+	id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+    );
