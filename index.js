3e69b669e1b12a147d4e6c965037b7d306114ef0
Jennifer Rosa <rosajen27@gmail.com>
2020-07-20 21:22:21 -0400

    create constantds (departments, roles, managers), and connect to localhost

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 const cTable = require('console.table');
 
+const departments = ["Sales", "Engineering", "Finance", "Legal"];
+const roles = ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Accountant", "Legal Team Lead", "Lawyer"];
+const managers = ["John Doe", "Ashley Rodriguez", "Malia Brown", "Sarah Lourd", "Kevin Tupik"];
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_DB"
+});
+
+
+
 inquirer.prompt([/* Pass your questions in here */], function( answers ) {
     // Use user feedback for... whatever!!
 });
